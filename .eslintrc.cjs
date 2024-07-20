e3963571db1c67bd58ebdc0a9147d3c41bcf08ea
Xyfalix <129175727+Xyfalix@users.noreply.github.com>
2024-07-20 19:26:24 +0800

    feat: install jest in prep for frontend tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,9 +20,17 @@ module.exports = {
   },
   overrides: [
     {
-      files: ["tests/**/*"],
+      files: ["**/*.test.js", "server.js"],
       env: {
         jest: true,
+      },
+      plugins: ["jest"],
+      rules: {
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error",
       },
     },
   ],
---
+++ src/components/NavBar.test.js
@@ -0,0 +1,19 @@
+// import { render, screen } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { createServer } from '../test/server';
+// import NavBar from './NavBar';
+
+// async function renderComponent() {
+//   render(
+//     <MemoryRouter>
+//       <NavBar />
+//     </MemoryRouter>
+//   );
+
+// }
+
+// describe("when user is not signed in", () => {
+//     test('Log In button is visible', () => {
+
+//     });
+// })
